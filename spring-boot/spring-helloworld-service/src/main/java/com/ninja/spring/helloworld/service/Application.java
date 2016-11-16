f8b7ea1b40149507c064ad644f3eecbe4685c430
Johan Larsson <johan.larsson@key-component.com>
2016-11-16 09:09:38 +0100

    Added the simplest sample

---
+++ spring-boot/spring-helloworld-service/src/main/java/com/ninja/spring/helloworld/service/Application.java
@@ -0,0 +1,13 @@
+package com.ninja.spring.helloworld.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
